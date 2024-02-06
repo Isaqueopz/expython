ef2d6d8189312e6f3df6f89f212166bce15270a9
Isaqueopz <sergioisaquepb@gmail.com>
2024-02-06 16:13:55 -0300

    estudando ainda

---
+++ 55.py
@@ -0,0 +1,32 @@
+for c in range (0,5):
+    p = float(input('Digite seu peso (KG):'))
+
+#valores maiores    
+if 0 > 1 and 0 > 2 and 0 > 3 and 0 > 4 and 0 > 5:
+    print('O maior peso foi {}'.format(0))
+elif 1 > 0 and 1 > 2 and 1 > 3 and 1 > 4 and 1 > 5:
+    print('O maior peso foi {}'.format(1))
+elif 2 > 0 and 2 > 1 and 2 > 3 and 2 > 4 and 2 > 5:
+    print('O maior peso foi {}'.format(2))
+elif 3 > 0 and 3 > 1 and 3 > 2 and 3 > 4 and 3 > 5:
+    print('O maior peso foi {}'.format(3))
+elif 4 > 0 and 4 > 1 and 4 > 2 and 4 > 3 and 4 > 5:
+    print('O maior peso foi {}'.format(4))
+elif 5 > 0 and 5 > 1 and 5 > 2 and 5 > 3 and 5 > 4:
+    print('O maior peso foi {}'.format(5))
+
+
+#valores menores 
+      
+if 0 < 1 and 0 < 2 and 0 < 3 and 0 < 4 and 0 < 5:
+    print('O menor peso foi {}'.format(0))
+elif 1 < 0 and 1 < 2 and 1 < 3 and 1 < 4 and 1 < 5:
+    print('O menor peso foi {}'.format(1))
+elif 2 < 0 and 2 < 1 and 2 < 3 and 2 < 4 and 2 < 5:
+    print('O menor peso foi {}'.format(2))
+elif 3 <  0 and 3 <  1 and 3 < 2 and 3 <  4 and 3 <  5:
+    print('O menor peso foi {}'.format(3))
+elif 4 < 0 and 4 <  1 and 4 <  2 and 4 <  3 and 4 <  5:
+    print('O menor peso foi {}'.format(4))
+elif 5 <  0 and 5 < 1 and 5 < 2 and 5 <  3 and 5 <  4:
+    print('O menor peso foi {}'.format(5))
