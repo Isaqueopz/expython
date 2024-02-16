603b18a11e3c0a0b758486e8ee57cfc4aff22e88
Isaqueopz <sergioisaquepb@gmail.com>
2024-02-16 09:39:31 -0300

    corrigindo o código

--- 56.py
+++ 56.py
@@ -1,26 +1,33 @@
-idades = 0
+somaidade = 0
 mediaidade = 0
 maioridadehomem = 0
 nomevelho = ''
 totmulher20 = 0
+mulher20 = ''
 for inf in range (1,5):
     print(8*'-',f'{inf}PESSOA',8*'-')
-    nome = str(input('Nome:'))
+    nome = str(input('Nome:')).strip()
     idade = int(input('Idade:'))
-    Sexo = str(input('Sexo [M/F]:'))
-    idades += idade
+    Sexo = str(input('Sexo [M/F]:')).strip()
+    somaidade += idade
     if inf == 1 and Sexo in 'Mm':
         maioridadedehomem = idade
         nomevelho = nome
     if Sexo in 'Mm'and idade > maioridadedehomem:
         maioridadedehomem = idade
         nomevelho = nome
-    if sexo in 'Ff' and idade < 20:
+    if Sexo in 'Ff' and idade < 20:
         totmulher20 += 1
-média = idades / 4
-print(f'Esta é a média do grupo{média}')
+    
+    
+mediaidade = somaidade / 4
+
+print(f'Esta é a média de idade do grupo {mediaidade}')
 print(f'O homem mais velho tem {maioridadedehomem} anos e se chama {nomevelho}.')
-print(f'Ao todo são {totmulher20} mulheres com menos de 20 anos')
+if Sexo in 'Ff' and totmulher20 == 0:
+        print('Não tem mulheres com menos de 20 anos na listagem!')
+else:
+     print(f'Ao todo são {totmulher20} mulheres com menos de 20 anos')
 
 
 
