0eec866df69af0854d9735271934b3cb4241af53
Isaqueopz <sergioisaquepb@gmail.com>
2024-02-20 12:10:52 -0300

    game de adivinhar ( corrigido)

--- 58.py
+++ 58.py
@@ -1,23 +1,25 @@
 #from random import randint (escolhe um número aleatório)
-from time import sleep
 from random import randint
-tentativa = 0
-t = 0
-lista = randint (0,10)
-print(20*'-=-')
-print('Vou pensar em um número de 0 a 10, qual será o número que eu pensei...')
-print(20*'-=-')
-sleep(2)
-p = int(input('Qual número eu pensei:'))
-if lista == p:
-    print('Olha só parabéns, você acertou de primeira!!!')
-else:
-    while tentativa != p:  
-        t += 1
-        tentativa = int(input('''Você errou, tente novamente!!!
-qual número deseja colocar agora:'''))
-        if tentativa == p:
-            print('Parabéns, agora você acertou!')
-        else: 
-            print('Tente novamente!')
-print (f'Você precisou de {t} para vencer')
+
+computador = randint (0,10)
+
+print('Estou pensando em alguns números.')
+print('Será que você consegue adivinhar...')
+
+count = 0
+
+acertou = False
+
+while not acertou:
+    palpite = palpite = int(input('Em qual número eu pensei?'))
+    count += 1
+    if palpite == computador:
+        acertou = True
+    else:
+        if palpite > computador:
+            print('Um pouco menos... tente novamente')
+        elif palpite < computador:
+            print('Um pouco mais... tente novamente')
+
+print(f'Acertou, precisando de {count} tentativas')
+    
