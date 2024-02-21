02ca16dcacbd92096d3711ea7c327f6461044ce5
Isaqueopz <sergioisaquepb@gmail.com>
2024-02-21 11:22:05 -0300

    Sequência de Fibonacci

--- 63.py
+++ 63.py
@@ -1,3 +1,16 @@
-elementos = int(input('Quantos elementos você quer ver da sequência ?'))
-v1 = 1 
-v2 = 1
+print('-'*30)
+print('Sequência de Fibonacci')
+print('-'*30)
+n = int(input('Quantos termos você quer mostrar?'))
+t1 = 0
+t2 = 1
+print('~'*30)
+print(f'{t1} > {t2}',end='')
+cont = 3 
+while cont <= n:
+    t3 = t1 + t2 
+    print(f' > {t3}',end='')
+    t1 = t2
+    t2 = t3 
+    cont += 1
+print (' > FIM')
