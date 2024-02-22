d25c6bc7408322a2e95f3011b91da1ab309dc8b3
Isaqueopz <sergioisaquepb@gmail.com>
2024-02-22 14:15:24 -0300

    gambiarra para FLAG

--- 64.py
+++ 64.py
@@ -1,16 +1,20 @@
 numero = int(input('Digite um valor:'))
 
-count = 0
+count = 0 
 soma = 0
 
 while numero != 999:
-    numero = int(input('Digite outro valor:'))
+    
 
     if numero == 999:
         print('Você preferiu sair!')
 
     elif numero != 999:
-        count =+ numero
-        
+        count += 1
+        soma += numero
+        numero = int(input('Digite um valor:'))    
     
-print(f'a quantidade de números até sair foram de {count}')
+    
+print(f'a quantidade de números até sair foram de {count} e a soma entre eles foi de {soma}')
+
+#para desconsiderar o FLAG, você coloca o input do lado de fora e outro dentro da condição na ultima linha 
