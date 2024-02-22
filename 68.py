a4dd214e886d1956c7dc8903810c4d23c7cf37bc
Isaqueopz <sergioisaquepb@gmail.com>
2024-02-22 16:09:07 -0300

    em alteração (impar ou par )

---
+++ 68.py
@@ -0,0 +1,25 @@
+import random
+
+impar = 'ímpar'
+par = 'Par'
+
+computador = random.choice([impar,par])
+
+escolha = ''
+
+print('='*55)
+print('Bem vindo ao game, você ira jogar contra o computador!')
+print('vamos ver que se sai melhor!... ')
+print('='*55)
+
+
+
+while True:
+    escolha = str(input('Qual a sua escolha?[par/impar]')).strip().upper()
+    if escolha == computador
+        print('Parabéns')
+
+
+
+
+  print(f'A escolha do computador foi {computador}')
