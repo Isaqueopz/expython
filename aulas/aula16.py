d7a7d9688fecf2045ed2719f666a4634db9ad672
Isaqueopz <sergioisaquepb@gmail.com>
2024-02-27 16:59:55 -0300

    (TUPLAS)

---
+++ aulas/aula16.py
@@ -0,0 +1,7 @@
+lanche = ('Hambúrguer','Suco','Pizza','Pudim')
+for cont in range (0,len(lanche)):
+    print(f'Eu vou comer {lanche[cont]} na posição {cont}')
+
+#for pos, comida in enumerate(lanche):
+#    print(f'Eu vou comer {comida} na posição {pos}')
+print('COMI PARA CARAMBA')
