7d481ba7b738ba184d21bb7a34d10039484e4925
Isaqueopz <sergioisaquepb@gmail.com>
2024-02-27 17:38:08 -0300

    TUPLAS

---
+++ 72.py
@@ -0,0 +1,2 @@
+tupla = ('Zero','Um','Dois','Três','Quatro','Cinco','Seis','Sete','Oito','Nove','Dez','Onze','Doze','Treze','Quartorze','Quinze','Dezesseis','Dezessete','Dezoito','Dezenove','Vinte')
+resp = ' '
