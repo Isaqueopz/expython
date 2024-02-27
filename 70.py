34eb51a51f3c53d7162d8d2f95a04e0260a2f468
Isaqueopz <sergioisaquepb@gmail.com>
2024-02-27 08:43:04 -0300

    ex estatistico com parada do if maior = maior

--- 70.py
+++ 70.py
@@ -1,32 +1,21 @@
-print ('-'*20)
-print ('LOJA SUPER BARATÃO')
-print ('-'*20)
-
-preço = 0
-total = 0 
-maior = 0
-continuar = ""
-produto = 0
-produto = produto < preço
-barato = 0
-
-while continuar != 'N':
-    nome = str(input('Nome do Produto: '))
-
-    preço = int(input('Preço: R$'))
-
+total = totmil = menor = cont = 0 
+barato = ''
+while True:
+    produto = str(input('Nome do Produto : '))
+    preço = float(input('Preço: R$ '))
+    cont += 1 
     total += preço
-
-    continuar = str(input('Quer continuar: [S/N] ')).upper().strip() [0]
-
     if preço > 1000:
-
-        maior += 1  
-    
-    
-    
-print (f'o total da compra foi R${total}')
-print (f'Temos {maior} produtos custando mais de R$1000.00')
-
-
-
+        totmil += 1
+    if cont == 1 or preço < menor:
+        menor = preço
+        barato = produto
+    resp = ' '
+    while resp not in 'SN':
+        resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    if resp == 'N':
+        break
+print('FIM DO PROGAMA')
+print(f'O total da compra foi R${total:.2f}')
+print(f'Temos {totmil} produtos custando mais de R$1000.00')
+print(f'O produto mais barato foi {barato} que custa R${menor:.2f}')
