5922e80d19b0b0bcf43a42e8d0b7b0dcaaafb71c
Isaqueopz <sergioisaquepb@gmail.com>
2024-02-27 19:02:34 -0300

    TUPLAS

---
+++ 74.py
@@ -0,0 +1,11 @@
+from random import randint
+num0 = randint(0,50)
+num1 = randint (0,50)
+num2 = randint (0,50)
+num3 = randint (0,50)
+num4 = randint (0,50)
+
+lista = (num0,num1,num2,num3,num4)
+print(f'Os valores escolhidos foram {lista}')
+print(f'o maior valor foi {max(lista)}')
+print(f'o menor valor foi {min(lista)}')
---
+++ 75.py
@@ -0,0 +1,17 @@
+val1 = int(input('Digite um valor: '))
+val2 = int(input('Digite um valor: '))
+val3 = int(input('Digite um valor: '))
+val4 = int(input('Digite um valor: '))
+
+lista = (val1,val2,val3,val4)
+cont9 = lista.count(9)
+inde3 = lista.index(3)
+
+
+
+
+print (f'Você digitou os valores {lista}')
+print(f'O número 9 apareceu {cont9} vezes')
+print(f'O número três apareceu na {inde3} posição')
+
+
