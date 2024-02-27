74e2df0c1867f88f9fef2eb1497792906ecb57ba
Isaqueopz <sergioisaquepb@gmail.com>
2024-02-27 08:04:09 -0300

    primeiro jogo de cadastro feito sozinho!!!!

--- 69.py
+++ 69.py
@@ -1,12 +1,21 @@
-idade = 0 
-sexo = ''
-continuar = 'Sim','sim'
-
-
+homem = 0
+idade1 = 0
+mulher = 0
 while True:
-    idade = int(input('Qual é a sua idade? '))
-    sexo = str(input('Qual é o seu sexo: [M/F]')).strip().upper()[0]
-    continuar = str(input('Você quer continuar? [SIM/NÃO] ')).strip().upper()[0]
-    if continuar == 'não':
+    idade = int(input('Qual é sua idade? '))
+    sexo = ' '
+    while sexo not in 'MF':
+        sexo = str(input('Qual é seu sexo? [M/F] ')).strip().upper()[0]
+        if sexo == 'M':
+            homem += 1
+        if idade >= 18:
+            idade1 += 1
+        if sexo in 'F' and idade < 20:
+            mulher += 1
+        continuar = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    if continuar != 'S':
         break
-         
+print (f'{idade1} tinham mais de 18 anos...')
+print (f'O total de homens foi de {homem} pessoas')
+print (f'O total de mulheres com menos de 20 anos foi(foram) de {mulher} mulher(es)')
+
