162b5427eda93923cf6e3cba48952a21b770b83d
Isaqueopz <sergioisaquepb@gmail.com>
2024-02-28 07:35:14 -0300

    num extenso com tuplas

--- 72.py
+++ 72.py
@@ -1,2 +1,12 @@
-tupla = ('Zero','Um','Dois','Três','Quatro','Cinco','Seis','Sete','Oito','Nove','Dez','Onze','Doze','Treze','Quartorze','Quinze','Dezesseis','Dezessete','Dezoito','Dezenove','Vinte')
-resp = ' '
+cont = ('Zero','Um','Dois','Três','Quatro','Cinco'
+         ,'Seis','Sete','Oito','Nove','Dez','Onze'
+         ,'Doze','Treze','Quartorze','Quinze'
+         ,'Dezesseis','Dezessete','Dezoito'
+         ,'Dezenove','Vinte')
+
+while True:
+    núm = int(input('Digite um número entre 0 e 20: '))
+    if 0 <= núm <= 20:
+        break
+    print('Tente novamente.', end='')
+print(f'Você digitou o número {cont[núm]}')
