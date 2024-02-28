b29a6116428f62d808ebd4746c86c4076be8ddd7
Isaqueopz <sergioisaquepb@gmail.com>
2024-02-28 10:20:52 -0300

    TABELA DE  PREÇOS

--- 76.py
+++ 76.py
@@ -1,20 +1,13 @@
-print('-'*50)
-print('{:.^}'.format('LISTAGEM DE PREÇOS'))
-print('-'*50)
-
-lis = ('Lápis',1.75,'Borracha',2.00,'Caderno',15.90,'Estojo',25.00,'Transferidor', 4.20 ,'Compasso',9.99,'Mochila',120.32,'Canetas',22.30,'Livro',34.90)
+lis = ('Lápis',1.75,'Borracha',2.00,'Caderno',15.90,'Estojo',25.00,'Transferidor'
+       , 4.20 ,'Compasso',9.99,'Mochila',120.32,'Canetas',22.30,'Livro',34.90)
 
 
+print('-'*40)
+print(f'{"LISTAGEM DE PREÇOS":^30}') # <<< lembrar de colocar aspas duplas para a formatação
+print('-'*40)
 
-print(f'{lis[0]} ............................... R$    {lis[1]:.2f}')
-print(f'{lis[2]} ............................ R$    {lis[3]:.2f}')
-print(f'{lis[4]} ............................ R$    {lis[5]:.2f}')
-print(f'{lis[6]} ............................. R$    {lis[7]:.2f}')
-print(f'{lis[8]} ........................ R$    {lis[9]:.2f}')
-print(f'{lis[10]}............................. R$    {lis[11]:.2f}')
-print(f'{lis[12]}............................ R$    {lis[13]:.2f}')
-print(f'{lis[14]}............................. R$    {lis[15]:.2f}')
-print(f'{lis[16]}............................... R$    {lis[17]:.2f}')
-print('-'*50)
-
-
+for pos in range (0, len(lis)):
+    if pos % 2 == 0:
+        print(f'{lis[pos]:.<30}', end='')
+    else:
+        print(f'R$  {lis[pos]:>.2f}')
