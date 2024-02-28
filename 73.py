f9dd6e92505657ffd4595d28b7f41bd17a5dbeda
Isaqueopz <sergioisaquepb@gmail.com>
2024-02-28 07:38:55 -0300

    tupla com #  add

--- 73.py
+++ 73.py
@@ -1,6 +1,6 @@
 times = ('Palmeiras','Grêmio','Atlético-mg','Flamengo','Botafogo','Red bull bragantino','Fluminense','Atletico-pr','Internacional','Fortaleza','São paulo','Cuiabá','Corinthians','Cruzeiro','Vasco','Bahia','Santos','Goiás','Coritiba','América-mg')
 
 print(f'os cinco primeiro colocados são {times[0:5]} ')
-print(f'os ultimo quatro colocados são {times[16:21]} ')
+print(f'os ultimo quatro colocados são {times[16:21]} ') #{times[-4:]}
 print(sorted(times))
 print('o time do vasco está na {} posição'.format(times.index('Vasco')))
