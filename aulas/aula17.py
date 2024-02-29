21b0629a08e37d68a941e36e7bd82de0a6cc7e01
Isaqueopz <sergioisaquepb@gmail.com>
2024-02-29 16:38:05 -0300

    estudando listagem e suas variáveis

---
+++ aulas/aula17.py
@@ -0,0 +1,27 @@
+# num = [2, 5, 9, 1]
+# num [2]= 3
+# num.append(7)
+# num.sort(reverse=True)
+# num.insert(2,0)
+# if 3 in num:
+#    num.remove(3)
+# else:
+#    print('Não achei o número 4')
+
+# print(num)
+# print(f'Essa lista tem {len(num)} elementos')
+
+# valores = []
+# valores.append(5)
+# valores.append(9)
+# valores.append(4)
+
+valores = list()
+for cont in range (0, 5):
+    valores.append(int(input('Digite um valor: ')))
+
+for c, v in enumerate(valores):
+    print(f'Na posição {c} encontrei o valor {v}!')
+print('Cheguei ao final da lista.')
+
+
