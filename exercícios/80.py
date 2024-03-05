10eeb414207b9ca3f52aa322a8cec77ea9a47295
Isaqueopz <sergioisaquepb@gmail.com>
2024-03-05 13:03:31 -0300

    lista ordenada com repetição

--- exercícios/80.py
+++ exercícios/80.py
@@ -1,1 +1,15 @@
-valores = list(range(0,5))
+lista = []
+for c in range(0, 5):
+    n = int(input('Digite um valor: '))
+    if c == 0 or n > lista[-1]:
+        lista.append(n)
+    else:
+        for pos in range (0,len(lista)):
+            numero_atual_da_lista = lista[pos]
+            if n <= lista[pos]:
+                lista.insert(pos, n)
+                break
+            
+print(f'Os valores digitados em ordem foram {lista}')
+                
+
