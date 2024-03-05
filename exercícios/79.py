d9fd79822ae462f32ba952a9a702f23b8658274a
Isaqueopz <sergioisaquepb@gmail.com>
2024-03-05 14:05:09 -0300

    valores unicos em uma lista

--- exercícios/79.py
+++ exercícios/79.py
@@ -1,18 +1,13 @@
-lista = list()
+números = list()
 while True:
-    lista.append(int(input('Digite um valor: ')))
-    pergunta = str(input('Você quer continuar [S/N]: ')).upper().strip()[0]
- 
-    if pergunta != 'S':
-     break
-    # if lista.append in list():
-    #     print('Valor repetido, não irá ser adicionado na lista!')
+    n = int(input('Digite um valor: '))
+    if n not in números:
+        números.append(n)
+    else:   
+        print('Esse valor ja foi digitado, digite novamente... ')
+    r = str(input('Quer continuar [S/N]? '))
+    if r in 'Nn':
+        break
 
-vorganizado = lista.sort()
-print(f'Essa é sua lista {lista}' , end='')
-
-
-# lista = [3, 1, 2]
-# vorganizado = lista.sort()
-# print(lista)  # Output: [1, 2, 3]
-# print(vorganizado)  # Output: None
+valororganizado = números.sort()
+print(f'{números}')
