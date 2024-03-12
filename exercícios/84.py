aa312ed40be9705e51a0a4a7f332092b5e66d9e2
Isaqueopz <sergioisaquepb@gmail.com>
2024-03-12 17:26:42 -0300

    Lista composta e análise de dados (MAI/MEN)

--- exercícios/84.py
+++ exercícios/84.py
@@ -1,28 +1,32 @@
 dados = list()
-pesad = pespes = peslev = quant = 0 
+pesad = mai = men = quant = 0 
 while True:
     inf = []
     nome = str(input('Qual seu nome: '))
-    inf.append(nome)
     peso = float(input('Qual é o seu peso: '))
+    inf.append(nome)
     inf.append(peso)
-    dados.append(inf[:]) 
+    if len(dados) == 0:
+        mai = men = inf[1]
+    else:
+        if inf [1] > mai:
+            mai = inf [1]
+        if inf [1] < men:
+            men = inf [1]
+    dados.append(inf[:])
+    inf.clear() 
     pesad += 1
     resp = str(input('Quer continuar [S/N]? ')).upper().strip()
-
     if resp in 'Nn':
         break
-    
-for p in dados:
-    if p[1] == max:
-        print(f'O maior peso foi {p[1]}kg peso do {p[0]}')
-    elif p[1] == min:
-        print(f'O menor peso foi {p[1]}kg peso do {p[0]}')
 
 print(f'Teve-se o total de {pesad} pessoas registradas')
-
-
-
-
-
-    
+print(f'O maior peso foi de {mai}KG. Peso de ',end='')
+for p in dados:
+    if p[1] == mai:
+        print(f'[{p[0]}] ')
+print(f'O menor peso foi de {men}KG. Peso de ', end='')
+for p in dados:
+    if p[1] == men:
+        print(f'[{p[0]}] ',end='')
+print()
