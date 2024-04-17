b377777ffeaef92f05eb68eba0d05ff9eabb629f
Isaqueopz <sergioisaquepb@gmail.com>
2024-04-17 09:40:43 -0300

    MÓDULOS/PACOTES

---
+++ aulas/aula22.py
@@ -0,0 +1,14 @@
+#MÓDULOS E PACOTES
+#--> dividir o código em pequenos pedaços ajudando em manutenção e legibilidade 
+from uteis import numeros
+
+
+
+num = int(input('Digite um valor: '))
+fat = numeros.fatorial(num)
+print(f'O fatorial de {num} é {fat}')
+print(f'O dobro de {num} é {numeros.dobro(num)}')
+
+
+#PACOTES
+# --> ajuda a separar os módulos em pacotes ajudando ainda mais na organização 
